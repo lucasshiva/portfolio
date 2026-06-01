790f2f20e14605b96df13cfcccd64decae646cfc
Lucas Silva <silva.lucasdev@gmail.com>
2026-05-31 22:46:21 -0300

    feat(about): add country flag

--- src/lib/components/about/About.svelte
+++ src/lib/components/about/About.svelte
@@ -1,3 +1,7 @@
+<script>
+  import IconBrazil from "~icons/twemoji/flag-brazil";
+</script>
+
 <section id="about" class="py-20 px-4 md:px-8 w-full bg-muted text-foreground">
   <div class="max-w-6xl mx-auto flex flex-col gap-10 md:gap-20">
     <h2 class="text-5xl md:text-6xl lg:text-[68px] text-center md:text-left leading-none">
@@ -5,10 +9,13 @@
     </h2>
     <div class="grid md:grid-cols-3 items-center md:items-start mb-16">
       <div class="flex flex-col gap-1 items-center md:items-start">
-        <img src="./me.png" alt="" class="size-80 object-cover object-top rounded-2xl" />
+        <img src="./me.png" alt="Me" class="size-80 object-cover object-top rounded-2xl" />
         <h3 class="font-bold mt-4 text-xl md:text-2xl">Lucas Cavalcante da Silva</h3>
         <p class="font-light text-lg md:text-xl">Software Developer</p>
-        <p class="font-light md:text-lg">São Paulo, Brazil</p>
+        <div class="flex gap-2 items-center">
+          <IconBrazil />
+          <p class="font-light md:text-lg">São Paulo, Brazil</p>
+        </div>
       </div>
       <div class="flex flex-col min-w-full px-8 pt-12 md:py-0 gap-8 md:col-span-2">
         <p class="text-xl md:text-2xl leading-relaxed text-foreground">
