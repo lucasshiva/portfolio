95bd67986baadaa5ed9c9a57e6eddbfe18efe183
Lucas Silva <silva.lucasdev@gmail.com>
2026-06-06 20:54:09 -0300

    feat: cache projects on prod

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,13 @@
-import { GITHUB_TOKEN } from "$env/static/private";
+import { GITHUB_TOKEN, GITHUB_USERNAME } from "$env/static/private";
+import { getCachedProjects, setCachedProjects } from "$lib/server/cache";
 import { fetchPortfolioRepos } from "$lib/server/github";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-  const user = "lucasshiva";
-  const projects = await fetchPortfolioRepos(user, GITHUB_TOKEN);
+  const cached = getCachedProjects();
+  if (cached) return { projects: cached };
+
+  const projects = await fetchPortfolioRepos(GITHUB_USERNAME, GITHUB_TOKEN);
+  setCachedProjects(projects);
   return { projects };
 };
