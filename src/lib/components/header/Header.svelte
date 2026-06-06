bccb0b57f48a5cc6836d3d2f72535bf758e90eca
Lucas Silva <silva.lucasdev@gmail.com>
2026-06-06 20:41:01 -0300

    refactor(header): use card color in border

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -3,7 +3,7 @@
   import MobileNav from "./MobileNav.svelte";
 </script>
 
-<header class="w-full border-b border-zinc-800">
+<header class="w-full border-b border-card">
   <DesktopNav class="hidden md:flex" />
   <MobileNav class="md:hidden" />
 </header>
