81ca809a3b6d885dc7cb18e1f982b9fa2f3884ae
Lucas Silva <silva.lucasdev@gmail.com>
2026-06-07 00:47:28 -0300

    feat(projects): make cards smaller on small screens

--- src/lib/components/projects/Projects.svelte
+++ src/lib/components/projects/Projects.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Project } from "$lib/types";
+  import IconInfo from "~icons/lucide/info";
   import ProjectCard from "./ProjectCard.svelte";
 
   type Props = {
@@ -13,13 +14,17 @@
   <div class="max-w-6xl mx-auto">
     <div class="text-center md:text-left">
       <h2 class="text-4xl md:text-5xl lg:text-6xl mb-4">Projects</h2>
-      <p class="text-muted-foreground mb-8 max-w-2xl text-lg md:text-xl">
-        A showcase of my projects
-      </p>
+      <div class="flex items-center justify-center md:justify-start gap-2 mb-8">
+        <IconInfo class="text-primary" />
+        <p class="text-muted-foreground max-w-2xl text-base md:text-lg">
+          Projects are automatically populated using GitHub's API.
+        </p>
+      </div>
     </div>
     {#if projects !== undefined && projects.length >= 1}
       <div class="projects">
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12">
+        <div
+          class="grid md:grid-cols-2 lg:grid-cols-3 max-w-sm sm:max-w-lg md:max-w-full mx-auto gap-10 mb-12 px-12 md:px-8 lg:px-0 py-8">
           {#each projects as project}
             <ProjectCard {project} />
           {/each}
